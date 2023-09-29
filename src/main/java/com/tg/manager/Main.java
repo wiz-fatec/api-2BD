4f86fe9cf26ad67a88f3a275d4b32e4b49770eaf
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2023-09-29 17:30:56 -0300

    🐛 connection data base
    connection data base

--- src/main/java/com/tg/manager/Main.java
+++ src/main/java/com/tg/manager/Main.java
@@ -1,7 +1,12 @@
 package com.tg.manager;
 
+import com.tg.manager.model.connection.ConnectionDataBase;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        ConnectionDataBase conect = new ConnectionDataBase();
+        conect.getConexao();
     }
 }
