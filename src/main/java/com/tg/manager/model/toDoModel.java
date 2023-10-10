5c9b8eb08b27611e2e6d890e1f3fab4902070aba
ChavesVini <vinidasilva2004@outlook.com>
2023-10-10 00:43:57 -0300

    :hammer: Add new class with connection
    add new class with connection

---
+++ src/main/java/com/tg/manager/model/toDoModel.java
@@ -0,0 +1,31 @@
+package com.tg.manager.model;
+
+import com.tg.manager.model.connection.ConnectionDataBase;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ToDoModel {
+    private String feedback;
+    private Integer notes;
+    private Integer idToDo;
+
+    public void addToDo(String feedbacks, Integer notes, Integer idToDo) {
+
+        try {
+            ConnectionDataBase connectionDb = new ConnectionDataBase();
+            Connection connection = connectionDb.getConexao();
+            String insercaoSQL = "INSERT INTO tg (feedbacks, notes, idToDo) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insercaoSQL);
+            preparedStatement.setString(1, feedbacks);
+            preparedStatement.setInt(2, notes);
+            preparedStatement.setInt(3, idToDo);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+            System.out.println("Dados inseridos com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
