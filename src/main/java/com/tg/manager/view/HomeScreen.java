c175e496d4b524f4d1f6c557f43bbcf2b11b50ca
Otavio Calderan <otaviocalderanadm@gmail.com>
2023-10-10 20:38:14 -0300

    ✨ (Feature): Create screen for CSV loading
    Create a screen for loading the CSV and decoding the file, transforming it into a String

---
+++ src/main/java/com/tg/manager/view/HomeScreen.java
@@ -0,0 +1,24 @@
+package com.tg.manager.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HomeScreen extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+  
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HomeScreen.fxml"));
+    Parent root = fxmlLoader.load();
+    Scene tela = new Scene(root);
+
+    primaryStage.setScene(tela);
+    primaryStage.setResizable(false);
+    primaryStage.show();
+  }
+}
