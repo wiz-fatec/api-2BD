057c0ce4419725c2e363d198dc8ab8321322ce97
Otavio Calderan <otaviocalderanadm@gmail.com>
2023-10-10 22:03:37 -0300

    🔀 (Fix): Change entry point for JavaFX application
    Moves the main function to the Main class, centralizing the initialization of the JavaFX application.

--- src/main/java/com/tg/manager/Main.java
+++ src/main/java/com/tg/manager/Main.java
@@ -1,6 +1,9 @@
 package com.tg.manager;
 
 import com.tg.manager.model.connection.ConnectionDataBase;
+import com.tg.manager.view.HomeScreen;
+
+import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +11,7 @@ public class Main {
         System.out.println("Hello world!");
         ConnectionDataBase conect = new ConnectionDataBase();
         conect.getConexao();
+
+        Application.launch(HomeScreen.class, args);
     }
 }
--- src/main/java/com/tg/manager/view/HomeScreen.java
+++ src/main/java/com/tg/manager/view/HomeScreen.java
@@ -7,9 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class HomeScreen extends Application {
-  public static void main(String[] args) {
-    launch(args);
-  }
   
   @Override
   public void start(Stage primaryStage) throws Exception {
