0562c5e38393d9758bf503c7f5589406717d2cb5
Otavio Calderan <otaviocalderanadm@gmail.com>
2023-10-31 21:19:53 -0300

    💄 Create portfolio report screen

---
+++ src/main/java/com/tg/manager/view/PortfolioReport.java
@@ -0,0 +1,21 @@
+package com.tg.manager.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PortfolioReport extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PortfolioReport"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+
+        primaryStage.setScene(tela);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/com/tg/manager/view/PortfolioReportController.java
@@ -0,0 +1,70 @@
+package com.tg.manager.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class PortfolioReportController {
+
+    @FXML
+    private ImageView ButtonCalendarPortfolio;
+
+    @FXML
+    private ImageView ButtonClipboardPortfolio;
+
+    @FXML
+    private ImageView ButtonHomePortfolio;
+
+    @FXML
+    private Button ButtonPDF;
+
+    @FXML
+    private Button ButtonReportGeneral;
+
+    @FXML
+    private Label emailFatec;
+
+    @FXML
+    private Label feedbackStudent;
+
+    @FXML
+    private Label gradeStudent;
+
+    @FXML
+    private Label nameStudent;
+
+    @FXML
+    private Label personalEmail;
+
+    @FXML
+    private Label tgRegistered;
+
+    @FXML
+    void DownloadCSVGeneral(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DownloadCSVSpecifc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToClipboardPortfolio(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GoToDeliveryScreenPortfolio(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GoToHomeScreenPortfolio(MouseEvent event) {
+
+    }
+
+}
